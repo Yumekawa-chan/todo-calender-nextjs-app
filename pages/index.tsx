238aaafcacc2c16396180bf88f5738ef9c12fb76
Yumekawa_chan <20aj079@ms.dendai.ac.jp>
2022-11-10 11:51:14 +0900

    add mypage access limit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,37 @@
 import { app2 } from "../hooks/firebase"
-import { getAuth, signOut } from "firebase/auth"
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth"
 import { useRouter } from "next/router"
+import React, { useState, useEffect } from "react";
+import { useNavigate, Navigate, NavigateFunction } from "react-router-dom";
 
 export default function Home() {
 
-const router = useRouter()
-const auth = getAuth(app2)
-const handleLogout = async () => {
-  await signOut(auth)
-  await router.push("/signup")
-}
+  // 表示名
+  const [user, setUser] = useState();
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
-  return(
-    <button onClick = {handleLogout}>ログアウト</button>
-  )
+  const router = useRouter()
+  const auth = getAuth(app2)
+  const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    await signOut(auth)
+    await router.push("/login")
+  }
+
+  if (user !== null){
+    return(
+      <>
+        <h1>マイページ</h1>
+        <p>{user && user.email}</p>
+        <button onClick = {handleLogout}>ログアウト</button>
+      </>
+    )
+  }else{
+    navigate("/login")
+  }
 }
