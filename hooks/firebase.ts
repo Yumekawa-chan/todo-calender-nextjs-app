a878a410b54634add77e34bdd0b13fa223b4fc27
Yumekawa_chan <20aj079@ms.dendai.ac.jp>
2022-11-11 17:16:34 +0900

    add firebase information

--- hooks/firebase.ts
+++ hooks/firebase.ts
@@ -1,10 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { useState } from "react";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCLET,
+  messagingSenderId: process.env.MESSAGENG_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -12,6 +19,9 @@ const auth = getAuth(app);
 export function useAuth() {
   return auth;
 }
+
+export const db = getFirestore();
+export const storage = getStorage();
 
 export function useUser() {
   const [user, setUser] = useState<User>();
